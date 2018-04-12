4946c8b8a5fd80b356bc7ff2761f0e68bf0135f8
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2018-04-12 13:55:55 +1200

    PyMC: Initial implementation of HKY likelihood (not working)

---
+++ hky.py
@@ -0,0 +1,47 @@
+import numpy as np
+import theano
+import theano.tensor as tt
+
+T, A, C, G = range(4)
+
+def make_tensor(x):
+	return theano.shared(np.array(x))
+
+def hky_transition_probs(alpha, beta, pi, t):
+	piY = pi[T] + pi[C]
+	piR = pi[A] + pi[G]
+	lambd = make_tensor([ # Eigenvalues
+		0,
+		-beta,
+		-(piY*beta + piR*alpha),
+		-(piY*alpha + piR*beta)		
+	])
+	U = make_tensor([ # Right eigenvectors as columns
+		[1, 1, 1, 1],
+		[1/piY, 1/piY, -1/piR, -1/piR],
+		[0, 0, pi[G]/piR, -pi[A]/piR],
+		[pi[C]/piY, -pi[T]/piY, 0, 0]
+	]).transpose()
+
+	Vt = make_tensor([ # Left eigenvectors as rows
+		[pi[T], pi[C], pi[A], pi[G]],
+		[piR*pi[T], piR*pi[C], -piY*pi[A], -piY*pi[G]],
+		[0, 0, 1, -1],
+		[1, -1, 0, 0]
+	])
+
+	return tt.dot(U, tt.dot(tt.diag(tt.exp(lambd * t)), Vt))
+
+# Start for one sequence
+# Then vectorise (tensor)
+def hky_loglike(alpha, beta, pi, t, x, y):
+	"""
+	alpha: Transition rate
+	beta: Tranversion rate
+	pi: Equilibrium frequencies
+	x, y: Sequences (encoded as integers)
+	t: Time
+	"""
+	Pt = hky_transition_probs(alpha, beta, pi, t)
+	return Pt[x, y]
+	

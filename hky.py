a3f0999d3f268ef7768f240a06de0d6e34687ee3
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2018-04-18 14:11:08 +1200

    PyMC: Vectorised implementation of HKY likelihood

--- hky.py
+++ hky.py
@@ -33,6 +33,38 @@ def hky_transition_probs(kappa, pi, t):
 
 	Q_unnormalised = tt.dot(U, tt.dot(tt.diag(lambd), Vt))
 	average_subs = -tt.nlinalg.trace(tt.dot(Q_unnormalised, tt.diag(pi)))
+	lambd_time_outer = tt.outer(t, lambd)
+	diag = tt.AllocDiag()(tt.exp(lambd_time_outer / average_subs))
+	U_dot_diag = tt.tensordot(U, diag, axes = 1)
+	U_dot_diag_dot_Vt = tt.tensordot(U_dot_diag.transpose(), Vt, axes = [1, 0]).dimshuffle(1, 2, 0)
+
+	return U_dot_diag_dot_Vt
+
+def hky_transition_probs_scalar(kappa, pi, t):
+	piY = pi[T] + pi[C]
+	piR = pi[A] + pi[G]
+	lambd = make_tensor([ # Eigenvalues 
+		0,
+		-1,
+		-(piY + piR*kappa),
+		-(piY*kappa + piR)		
+	])
+	U = make_tensor([ # Right eigenvectors as columns (rows of transpose)
+		[1, 1, 1, 1],
+		[1/piY, 1/piY, -1/piR, -1/piR],
+		[0, 0, pi[G]/piR, -pi[A]/piR],
+		[pi[C]/piY, -pi[T]/piY, 0, 0]
+	]).transpose()
+
+	Vt = make_tensor([ # Left eigenvectors as rows
+		[pi[T], pi[C], pi[A], pi[G]],
+		[piR*pi[T], piR*pi[C], -piY*pi[A], -piY*pi[G]],
+		[0, 0, 1, -1],
+		[1, -1, 0, 0]
+	])
+
+	Q_unnormalised = tt.dot(U, tt.dot(tt.diag(lambd), Vt))
+	average_subs = -tt.nlinalg.trace(tt.dot(Q_unnormalised, tt.diag(pi)))
 
 	return tt.dot(U, tt.dot(tt.diag(tt.exp(lambd / average_subs * t)), Vt))
 

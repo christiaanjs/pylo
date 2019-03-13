9eeeed71c6f408fdcf059b49ce58a51598653176
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-03-13 16:52:55 +1300

    Pymc: Rewrote tree topology for serial sampling

--- pylo/topology.py
+++ pylo/topology.py
@@ -1,60 +1,106 @@
-from pylo.transform import get_parent_indices, get_tables_np, get_heights, get_leaf_mask
 import numpy as np
 import theano
 import theano.tensor as tt
 
+def traverse_postorder(node, leaf_f, node_f, transform=False):
+    if node.is_leaf:
+        return np.array([leaf_f(node)])
+    else:
+        child_results = [traverse_postorder(child, leaf_f, node_f, transform=transform) for child in node.descendants]
+        if transform:
+            node_result, child_results = node_f(node, child_results) 
+        else:
+            node_result = node_f(node, child_results)
+        return np.concatenate(child_results + [[node_result]])
+
+def get_heights(node):
+    leaf_f = lambda x: 0.0
+    def node_f(node, child_heights):
+        child_branch_lengths = [child.length for child in node.descendants]
+        child_branch_heights = [single_child_heights[-1] + branch_length for single_child_heights, branch_length in zip(child_heights, child_branch_lengths)]
+        node_height = max(child_branch_heights)
+        adjusted_child_heights = [single_child_heights + (node_height - child_branch_height) for single_child_heights, child_branch_height in zip(child_heights, child_branch_heights)]
+        return node_height, adjusted_child_heights
+    return traverse_postorder(node, leaf_f, node_f, transform=True)
+
+def get_max_leaf_descendant_heights(node):
+    leaf_f = lambda x: [0.0, 0.0]
+    def node_f(node, child_results):
+        child_branch_lengths = [child.length for child in node.descendants]
+        child_branch_heights = [single_child_results[-1, 0] + branch_length for single_child_results, branch_length in zip(child_results, child_branch_lengths)]
+        node_height = max(child_branch_heights)
+        adjusted_child_results = [single_child_results + (node_height - child_branch_height) for single_child_results, child_branch_height in zip(child_results, child_branch_heights)]
+        max_leaf_descendant_height = max(adjusted_result[-1, 1] for adjusted_result in adjusted_child_results)
+        return [node_height, max_leaf_descendant_height], adjusted_child_results
+    return traverse_postorder(node, leaf_f, node_f, transform=True)[:, 1]
+
+def get_leaf_mask(node):
+    return traverse_postorder(node, lambda x: True, lambda x, y: False)
+
+def get_parent_indices(node):
+    leaf_f = lambda x: -1
+    def node_f(node, child_results):
+        lengths = np.array([len(child_result) for child_result in child_results])
+        node_index = np.sum(lengths)
+        adjustments = np.concatenate(([0], np.cumsum(lengths[:-1])))
+        adjusted_child_results = [np.concatenate((child_result[:-1] + adjustment, [node_index])) for child_result, adjustment in zip(child_results, adjustments)]
+
+        return -1, adjusted_child_results
+    return traverse_postorder(node, leaf_f, node_f, transform=True)
+
+def get_child_indices(node): # Assumes binary trees
+    leaf_f = lambda x: [-1, -1]
+    def node_f(node, child_results):
+        lengths = np.array([len(child_result) for child_result in child_results])
+        adjustments = np.concatenate(([0], np.cumsum(lengths[:-1])))
+        node_children = np.cumsum(lengths) - 1 
+        adjusted_child_results = [np.where(child_result == -1, -1, child_result + adjustment) for child_result, adjustment in zip(child_results, adjustments)]
+        return node_children, adjusted_child_results
+    return traverse_postorder(node, leaf_f, node_f, transform=True)
+
+def get_names(node):
+    leaf_f = lambda x: x.name
+    node_f = lambda x, y: None
+    return traverse_postorder(node, leaf_f, node_f)
+
 class TreeTopology(object):
     def __init__(self, tree):
         self.tree = tree
-        _, self.leaf_children, self.leaf_mask, self.child_indices = get_tables_np(tree)
-        self.parent_indices = np.array(get_parent_indices(self.child_indices))
+        self.names = get_names(tree)
+        self.leaf_mask = get_leaf_mask(tree)
+        self.node_mask = np.logical_not(leaf_mask)
+        self.child_indices = get_child_indices(tree)
+        self.parent_indices = get_parent_indices(tree)
 
     def get_init_heights(self):
         return get_heights(self.tree)
     
-    def get_internal_mask(self):
-        return np.logical_not(get_leaf_mask(self.tree))
-    
     def build_sequence_table(self, sequence_dict, dummy_seq=None):
         if dummy_seq is None:
             dummy_seq = list(sequence_dict.values())[0]
-        
-        return [[dummy_seq if name is None else sequence_dict[name] for name in node] for node in self.leaf_children]
+               
+        node_child_indices = self.child_indices[self.leaf_mask]
+        leaf_child_names = np.where(self.leaf_mask[node_child_indices], self.names[node_child_indices], None)        
 
-    def get_heights(self, root_height, non_root_proportions):
-        n_internal_nodes = len(self.leaf_children)
-        parent_indices_reversed = self.parent_indices[::-1]
-        parent_indices_inorder = np.where(parent_indices_reversed == -1, -1, n_internal_nodes - parent_indices_reversed - 1)
-        parent_proportions = tt.concatenate([non_root_proportions, tt.ones(1)])
-        parent_proportions_inorder = parent_proportions[::-1]
-        func = lambda i, parent, prop, out: tt.set_subtensor(out[i], tt.where(tt.eq(parent, -1), prop, prop*out[parent]))
-        ixs = tt.arange(n_internal_nodes)
-        out_init = tt.zeros(n_internal_nodes)
-        heights_inorder = theano.scan(
-            func,
-            sequences=(ixs, tt.as_tensor(parent_indices_inorder), parent_proportions_inorder),
-            outputs_info=out_init)[0][-1]
-        return root_height*heights_inorder[::-1] 
+        return [[dummy_seq if name is None else sequence_dict[name] for name in node] for node in leaf_child_names]
 
     def get_proportions(self, heights):
         root_height = heights[-1]
         non_root_heights = heights[:-1]
-        non_root_parent_indices = self.parent_indices[:-1]
+        non_root_parent_indices = self.parent_indices[self.node_mask][:-1]
         parent_heights = heights[non_root_parent_indices]
         return non_root_heights/parent_heights, root_height
 
-
     def get_child_branch_lengths(self, heights):
-        child_heights = tt.where(tt.eq(self.child_indices, -1), 0.0, heights[self.child_indices])
+        child_heights = heights[self.child_indices[self.node_mask]]
         return heights.dimshuffle(0, 'x') - child_heights
 
-
     def get_internal_node_count(self):
-        return len(self.leaf_children)
+        return np.sum(self.node_mask)
 
     def get_taxon_count(self):
-        return self.get_internal_node_count() + 1
+        return np.sum(self.leaf_mask)
 
     def get_root_index(self):
-        return self.get_internal_node_count() - 1
+        return get_internal_node_count()  - 1
 
--- pylo/transform.py
+++ pylo/transform.py
@@ -12,76 +12,9 @@ def encode_sequences(taxa_dict):
 def get_dummy_seq(taxa_dict):
     return np.repeat(GAP, len(list(taxa_dict.values())[0])) 
 
-def process_child_result(child, child_result):
-    branch_lengths, _, _, _ = child_result
-    child_leaf = len(branch_lengths) == 0
-    if child_leaf:
-        return child.length, child.name, True
-    else:
-        return child.length, None, False
-
 def group_sequences(taxa_dict):
     taxon_names = list(taxa_dict.keys())
     taxa_df = pd.DataFrame(taxa_dict)
     pattern_series = taxa_df.groupby(taxon_names).size()
     pattern_dict = pattern_series.index.to_frame().to_dict(orient='list')
-    return pattern_dict, pattern_series.values
 
-def reverse_cumsum(x):
-    sum_ = 0
-    res = [0]*len(x)
-    res[-1] = x[-1]
-    for i in range(len(x) - 2, -1, -1):
-        res[i] = x[i] + res[i + 1]
-    return res
-    
-def get_tables_offset(node):
-    children = node.descendants
-    if(len(children) == 0):
-        return [], [], [], []
-    else:
-        child_results = [get_tables_offset(child) for child in children]
-        branch_length_results, leaf_children_results, leaf_results, offset_results = zip(*child_results)
-        
-        branch_lengths, leaf_children, leaf_mask = zip(*[process_child_result(child, result) for child, result in zip(children, child_results)])
-        child_offsets = [DUMMY_INDEX if is_leaf else x for x, is_leaf in zip(reverse_cumsum([len(x) for x in offset_results[1:]] + [1]), leaf_mask)]
-
-        return (list_concat(branch_length_results, list(branch_lengths)),
-            list_concat(leaf_children_results, list(leaf_children)),
-            list_concat(leaf_results, list(leaf_mask)),
-            list_concat(offset_results, child_offsets))
-
-def get_tables(node):
-    branch_lengths, leaf_children, leaf_mask, offsets = get_tables_offset(node)
-    indices = [ [DUMMY_INDEX if is_leaf else i - offset for offset, is_leaf in zip(offset_row, leaf_row) ] for i, (offset_row, leaf_row) in enumerate(zip(offsets, leaf_mask)) ]
-    return branch_lengths, leaf_children, leaf_mask, indices
-
-def get_tables_np(node):
-    return tuple([np.array(x) for x in get_tables(node)])
-
-def get_parent_indices(child_indices):
-    n_internal_nodes = len(child_indices)
-    parent_indices = [-1]*n_internal_nodes
-    for i in range(n_internal_nodes):
-        for child_index in child_indices[i]:
-            if child_index != -1:
-                parent_indices[child_index] = i
-    return parent_indices
-
-def get_heights(node):
-    if node.is_leaf:
-        return np.array([0.0])
-    else:
-        child_heights = [get_heights(child) for child in node.descendants]
-        child_branch_lengths = [child.length for child in node.descendants]
-        child_branch_heights = [single_child_heights[-1] + branch_length if len(single_child_heights) > 0 else branch_length for single_child_heights, branch_length in zip(child_heights, child_branch_lengths)]
-        node_height = max(child_branch_heights)
-        adjusted_child_heights = [single_child_heights + (node_height - child_branch_height) for single_child_heights, child_branch_height in zip(child_heights, child_branch_heights)]
-        return np.concatenate(adjusted_child_heights + [[node_height]])
-       
-def get_leaf_mask(node):
-    if node.is_leaf:
-        return [True]
-    else:
-        return np.concatenate([get_leaf_mask(child) for child in node.descendants] + [[False]])
-     

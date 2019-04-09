e43a6bc34d140b8ba6deedf93b0a40b06c36451d
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-04-09 15:39:39 +1200

    Pymc: Template for BEAST analysis

--- eval/build_templates.py
+++ eval/build_templates.py
@@ -15,7 +15,7 @@ template_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 tree_sim_template_file = 'sim-tree.j2.xml'
 tree_sim_out_file = 'sim-tree.xml'
 
-n_taxa = 100
+n_taxa = 10
 min_pop_size = 1000
 max_pop_size = 2000
 sampling_window = 100
@@ -44,6 +44,7 @@ tree_sim_result_path = tree_sim_out_path.with_suffix('.trees')
 seq_sim_out_file = 'sim-seq.xml'
 
 sequence_length = 20
+relaxed_clock = False
 mutation_rate = 3.0
 kappa = 2.0
 frequencies = [0.25, 0.25, 0.25, 0.25]
@@ -58,6 +59,7 @@ seq_sim_template = template_env.get_template(seq_sim_template_file)
 seq_sim_string = seq_sim_template.render(
     taxon_names=taxon_names,
     newick_string=newick_string,
+    relaxed_clock=relaxed_clock,
     sequence_length=sequence_length,
     mutation_rate=mutation_rate,
     kappa=kappa,
@@ -75,4 +77,42 @@ seq_sim_result_file = 'sequences.xml'
 
 import xml
 seq_xml_root = xml.etree.ElementTree.parse(out_path / seq_sim_result_file)
-sequence_dict = { tag.attrib['taxon']: tag.attrib['value'] for tag in seq_xml_root.find('./sequence') }
+sequence_dict = { tag.attrib['taxon']: tag.attrib['value'] for tag in seq_xml_root.findall('./sequence') }
+
+##################
+# BEAST Analysis #
+##################
+
+# TODO: log_every
+# prior_params[clock_rate, kappa, pop_size][m, s]i (1.0, 1.25)
+# init_values
+# estimate_topology
+
+chain_length = int(1e6)
+log_every = 1000
+prior_params = {
+    'clock_rate': { 'm': 1.0, 's': 1.25 },
+    'pop_size': { 'm': 1.0, 's': 1.25 },
+    'kappa': { 'm': 1.0, 's': 1.25 }
+}
+init_values = { 'clock_rate': 1.0, 'pop_size': 1.0, 'kappa': 2.0 }
+estimate_topology = False
+
+beast_analysis_template_file = 'beast-analysis.j2.xml'
+beast_analysis_out_file = 'beast-analysis.xml'
+
+# TODO: Re-initialise tree heights
+
+beast_analysis_template = template_env.get_template(beast_analysis_template_file)
+beast_analysis_string = beast_analysis_template.render(
+    newick_string=newick_string,
+    sequence_dict=sequence_dict,
+    date_trait_string=date_trait_string,
+    chain_length=chain_length,
+    log_every=log_every,
+    init_values=init_values,
+    prior_params=prior_params
+)
+
+with open(out_path / beast_analysis_out_file, 'w') as f:
+    f.write(beast_analysis_string)

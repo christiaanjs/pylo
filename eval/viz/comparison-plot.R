9bda5871aa914fb234154478afd6caa4274c1b77
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-04-30 16:53:04 +1200

    Pymc: Visualisation of eval results

---
+++ eval/viz/comparison-plot.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(dplyr)
+
+outDir <- file.path('..', 'out')
+runOutDirs <- file.path(outDir, dir(outDir))
+
+summaries <- file.path(runOutDirs, 'run_summary.pickle') %>% lapply(yaml::read_yaml)
+seeds <- summaries %>% lapply(function(x) x$config$seed)
+
+results <- file.path(runOutDirs, 'results.csv') %>% lapply(readr::read_csv)
+df <- results %>% setNames(seeds) %>% bind_rows(.id = 'seed')
+
+
+withTime <- df %>%
+  group_by(method) %>% 
+  mutate(elapsed_time = date_time - min(date_time)) %>% 
+  ungroup()
+
+ggplot(withTime, aes(x = elapsed_time, y = error, colour = method)) +
+  geom_point()

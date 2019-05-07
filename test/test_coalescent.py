c273277c042a38ffe4b80a3efe5e242acbfda07e
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-05-08 11:03:26 +1200

    More tests for coalescent

--- test/test_coalescent.py
+++ test/test_coalescent.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 import newick
 from numpy.testing import assert_allclose
@@ -16,7 +17,14 @@ def test_coalescent_constant_population():
     logp_expected = -(4 / pop) - 2 * np.log(pop)
     assert_allclose(logp, logp_expected, rtol=1e-3)
     
-    
-    
-    
-    
+test_data = [(123,-14.456065261239203),(999,-20.722666738348064)]
+@pytest.mark.parametrize('pop,logp_expected', test_data)
+def test_coalescent_heterochronous(pop,logp_expected):
+    newick_string = "(((A:0.2,B:0.1):0.3,C:0.1):0.3,D:0.8)"
+    tree = newick.loads(newick_string)[0]
+    topology = TreeTopology(tree)
+    population_function = ConstantPopulationFunction(topology, pop)
+    height_dist = CoalescentTree.dist(topology, population_function)
+    height_values = topology.get_init_heights()[topology.node_mask]
+    logp = height_dist.logp(height_values).eval()
+    assert_allclose(logp, logp_expected, rtol=1e-3)

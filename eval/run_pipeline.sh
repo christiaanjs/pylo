5e82f29349a0639713bc0d5965b9667686fe235b
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-05-08 17:36:51 +1200

    Script to run whole pipeline

---
+++ eval/run_pipeline.sh
@@ -0,0 +1,4 @@
+python pipeline.py config.yaml sim_pipeline
+python pipeline.py config.yaml variational_pipeline
+python pipeline.py config.yaml results_pipeline
+python pipeline.py config.yaml trace_pipeline

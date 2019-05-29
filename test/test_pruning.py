2074a9fb210bd1b49b96df797f9b5dff6f7b4416
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-05-29 15:06:23 +1200

    Fix main likelihood test for new structure

--- test/test_pruning.py
+++ test/test_pruning.py
@@ -1,39 +1,40 @@
 from pylo.pruning import phylogenetic_log_likelihood
-from pylo.transform import get_tables, group_sequences
-from pylo.hky import hky_transition_probs_mat
+from pylo.transform import group_sequences
+from pylo.hky import HKYSubstitutionModel
+from pylo.topology import TreeTopology
 
 import numpy as np
 import theano.tensor as tt
 import theano
 from numpy.testing import assert_allclose
-import numdifftools as nd
 
-GRAD_RTOL = 1e-3
-
-def test_pruning(taxa_encoded, tree):
+def test_pruning_value(taxa_encoded, tree):
     taxa_patterns, pattern_frequencies = group_sequences(taxa_encoded)
     pattern_frequencies = np.array(pattern_frequencies)
 
-    dummy_seq = np.repeat(-1, len(list(taxa_patterns.values())[0]))
-    child_branch_lengths, child_sequences, child_leaf_mask, child_indices = get_tables_np(tree, taxa_patterns, dummy_seq)
+    topology = TreeTopology(tree)
+    child_sequences = topology.build_sequence_table(taxa_patterns)
+    node_heights = topology.get_init_heights()[topology.node_mask]
 	
     kappa = 1.0
     pi = np.ones(4)/4
     
     kappa_ = tt.scalar()
     pi_ = tt.vector()
-    child_branch_lengths_ = tt.matrix()
+    node_heights_ = tt.vector()
+    child_branch_lengths_ = topology.get_child_branch_lengths(node_heights_)
     child_sequences_ = tt.tensor3(dtype='int64')
     child_leaf_mask_ = tt.matrix(dtype='bool')
     child_indices_ = tt.matrix(dtype='int64')
     pattern_frequencies_ = tt.vector(dtype='int64')
 
-    child_transition_probs_ = hky_transition_probs_mat(kappa_, pi_, child_branch_lengths_)
+    substitution_model = HKYSubstitutionModel(kappa_, pi_)
+    child_transition_probs_ = substitution_model.get_transition_probs(child_branch_lengths_)
 	
     ll_ = phylogenetic_log_likelihood(child_indices_, child_transition_probs_, child_sequences_, child_leaf_mask_, pattern_frequencies_, pi_)
     
-    f = theano.function([kappa_, pi_, child_branch_lengths_, child_indices_, child_sequences_, child_leaf_mask_, pattern_frequencies_], ll_)
-    res = f(kappa, pi, child_branch_lengths, child_indices, child_sequences, child_leaf_mask, pattern_frequencies)
+    f = theano.function([kappa_, pi_, node_heights_, child_indices_, child_sequences_, child_leaf_mask_, pattern_frequencies_], ll_)
+    res = f(kappa, pi, node_heights, topology.node_child_indices, child_sequences, topology.get_node_child_leaf_mask(), pattern_frequencies)
     
     assert_allclose(res, -1992.2056440317247)
 

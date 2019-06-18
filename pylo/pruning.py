564d0ff12d211100a30b6f0654fe892a715b1d7f
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-06-18 18:31:33 -0400

    Implement pruning on log scale

--- pylo/pruning.py
+++ pylo/pruning.py
@@ -4,6 +4,10 @@ from theano.ifelse import ifelse
 from pylo.common import GAP
 from pymc3.distributions import Discrete
 from pymc3 import Potential
+import pymc3 as pm
+
+def logsumexp(x, axis=None):
+    return pm.math.logsumexp(x, axis=axis).take(0, axis=axis)
 
 # For stateless children, use dummy sequence
 # For stateful children, use dummy child indices
@@ -14,17 +18,17 @@ def make_partial_probabilities(child_indices, child_transition_probs, child_sequ
 		# child_partials [child, site, child_char]
 		child_partials_shuffled = child_partials.dimshuffle(0, 1, 'x', 2) # [child, site, (parent_char), child_char]
 		transition_probs_shuffled = child_transition_probs.dimshuffle(0, 'x', 1, 2) # [child, (site), parent_char, child_char]
-		return (child_partials_shuffled * transition_probs_shuffled).sum(axis=3)
+		return logsumexp(child_partials_shuffled + transition_probs_shuffled, axis=3)
 
 	def partials_from_sequences(child_sequences, child_transition_probs):
 		# child_transition_probs [child, parent_char, child_char] 
 		children = tt.arange(child_sequences.shape[0]).dimshuffle(0, 'x') # [child, (site)]
-		return tt.switch(tt.eq(child_sequences, GAP).dimshuffle(0, 1, 'x'), 1.0, child_transition_probs[children, :, child_sequences]) # [child, site, parent_char]
+		return tt.switch(tt.eq(child_sequences, GAP).dimshuffle(0, 1, 'x'), 0.0, child_transition_probs[children, :, child_sequences]) # [child, site, parent_char]
 
 	def fill_row(node_index, child_indices, child_sequences, child_transition_probs, child_leaf_mask, partial_probs):
 		leaf_child_partials = partials_from_sequences(child_sequences, child_transition_probs)
 		node_child_partials = partials_from_partials(partial_probs[child_indices], child_transition_probs)
-		return tt.set_subtensor(partial_probs[node_index], tt.switch(child_leaf_mask.dimshuffle(0, 'x', 'x'), leaf_child_partials, node_child_partials).prod(axis=0))
+		return tt.set_subtensor(partial_probs[node_index], tt.switch(child_leaf_mask.dimshuffle(0, 'x', 'x'), leaf_child_partials, node_child_partials).sum(axis=0))
 		
 	partial_probs_filled = theano.scan(fill_row,
 		sequences=[tt.arange(child_indices.shape[0]), child_indices, child_sequences, child_transition_probs, child_leaf_mask],
@@ -33,11 +37,11 @@ def make_partial_probabilities(child_indices, child_transition_probs, child_sequ
 	return partial_probs_filled
 
 def phylogenetic_log_likelihood(child_indices, child_transition_probs, child_patterns, child_leaf_mask, pattern_frequencies, character_frequencies):
-    partials = make_partial_probabilities(child_indices, child_transition_probs, child_patterns, child_leaf_mask) # [node, site, char]
+    partials = make_partial_probabilities(child_indices, tt.log(child_transition_probs), child_patterns, child_leaf_mask) # [node, site, char]
     root_partials = partials[-1] #[site, char]
     char_freqs_reshuffled = character_frequencies.dimshuffle('x', 0) #[(site), char]
-    site_probs = (root_partials * char_freqs_reshuffled).sum(axis=1)
-    return (tt.log(site_probs) * pattern_frequencies).sum()
+    site_logprobs = logsumexp(root_partials + tt.log(char_freqs_reshuffled), axis=1)
+    return (site_logprobs * pattern_frequencies).sum()
     
 def LeafSequences(name, topology, substitution_model, child_distances, child_patterns, pattern_frequencies, *args, **kwargs):
     transition_probs = substitution_model.get_transition_probs(child_distances)

69dc1518564ebfa0b6c47d54d3fcc89d0cbc427a
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-07-11 17:49:38 -0700

    Working skygrid implementation

--- pylo/tree/coalescent.py
+++ pylo/tree/coalescent.py
@@ -4,6 +4,8 @@ from pylo.tree.transform import TreeHeightProportionTransform
 from pymc3.distributions import Continuous
 from pymc3.util import get_variable_name
 
+COALESCENCE, SAMPLING, OTHER = -1, 1, 0
+
 def coalescent_likelihood(lineage_count,
                           population_func, # At coalescence
                           population_areas, # Integrals of 1/N
@@ -11,8 +13,8 @@ def coalescent_likelihood(lineage_count,
     k_choose_2 = lineage_count * (lineage_count - 1) * 0.5
     return -tt.sum(k_choose_2 * population_areas) - tt.sum(tt.log(population_func[coalescent_mask]))
 
-def get_lineage_count(node_mask):
-    return tt.cumsum(tt.where(node_mask, -1, 1))
+def get_lineage_count(event_types):
+    return tt.cumsum(event_types)
 
 class PopulationFunction:
     def make_intervals(heights_sorted, node_mask): #returns lineage_count, population_func, population_areas, coalescent_mask
@@ -24,13 +26,45 @@ class ConstantPopulationFunction(PopulationFunction):
         self.topology = topology
         
     def make_intervals(self, heights_sorted, node_mask):
-        lineage_count = get_lineage_count(node_mask)[:-1]
+        lineage_count = get_lineage_count(tt.where(node_mask, COALESCENCE, SAMPLING))[:-1]
         population_func_array = tt.alloc(self.population_size, self.topology.get_node_count() - 1)
         durations = heights_sorted[1:] - heights_sorted[:-1]
         population_areas = durations / self.population_size
         coalescent_mask = node_mask[1:]
         return lineage_count, population_func_array, population_areas, coalescent_mask
-        
+
+class GridPopulationFunction(PopulationFunction): # Piecewise constant changing over grid
+    def __init__(self, topology, population_func, max_height, grid_size):
+        self.population_func = population_func
+        self.topology = topology
+        self.max_height = max_height
+        self.grid_size = grid_size
+
+    def make_intervals(self, heights_sorted, node_mask):
+        grid_times = self.max_height / self.grid_size * (np.arange(self.grid_size) + 1)
+
+        event_types_unsorted = tt.concatenate((tt.alloc(OTHER, self.grid_size + 1), tt.where(node_mask, COALESCENCE, SAMPLING)))
+        times_unsorted = tt.concatenate((grid_times, [np.inf], heights_sorted))
+        pop_sizes_unsorted = tt.concatenate((self.population_func, tt.fill(node_mask, np.nan)))
+        sort_indices = tt.argsort(times_unsorted, kind='stable')
+        indices = np.arange(self.topology.get_node_count() + self.grid_size + 1)
+
+        times_sorted = times_unsorted[sort_indices]
+        time_mask = times_sorted <= heights_sorted[-1]
+        times_to_use = times_sorted[time_mask]
+
+
+        event_types_sorted = event_types_unsorted[sort_indices]
+        event_types_to_use = event_types_sorted[time_mask]
+        lineage_count = get_lineage_count(event_types_to_use)[:-1]
+        coalescent_mask = tt.eq(event_types_to_use[1:], COALESCENCE)
+
+        pop_sizes_null = pop_sizes_unsorted[sort_indices]
+        notnull_indices = (~tt.isnan(pop_sizes_null) & (indices[:, np.newaxis] <= indices[np.newaxis, :])).argmax(axis=1)
+        pop_sizes = pop_sizes_null[notnull_indices][time_mask][1:]
+        durations = times_to_use[1:] - times_to_use[:-1]
+        population_areas = durations / pop_sizes 
+        return lineage_count, pop_sizes, population_areas, coalescent_mask 
     
 class CoalescentTree(Continuous):
     def __init__(self, topology, population_func, *args, **kwargs):
--- test/test_coalescent.py
+++ test/test_coalescent.py
@@ -3,7 +3,11 @@ import numpy as np
 import newick
 from numpy.testing import assert_allclose
 from pylo.topology import TreeTopology
-from pylo.tree.coalescent import CoalescentTree, ConstantPopulationFunction
+from pylo.tree.coalescent import CoalescentTree, ConstantPopulationFunction, GridPopulationFunction
+
+@pytest.fixture
+def heterochronous_newick():
+    return "(((A:0.2,B:0.1):0.3,C:0.1):0.3,D:0.8)"
 
 def test_coalescent_constant_population():
     pop = 10000
@@ -19,9 +23,8 @@ def test_coalescent_constant_population():
     
 test_data = [(123,-14.446309163678226),(999,-20.721465537146862)]
 @pytest.mark.parametrize('pop,logp_expected', test_data)
-def test_coalescent_heterochronous(pop,logp_expected):
-    newick_string = "(((A:0.2,B:0.1):0.3,C:0.1):0.3,D:0.8)"
-    tree = newick.loads(newick_string)[0]
+def test_coalescent_heterochronous(pop,logp_expected,heterochronous_newick):
+    tree = newick.loads(heterochronous_newick)[0]
     topology = TreeTopology(tree)
     population_function = ConstantPopulationFunction(topology, pop)
     height_dist = CoalescentTree.dist(topology, population_function)
@@ -39,3 +42,20 @@ def test_coalescent_dengue(dengue_config, pop_size, logp_expected):
     height_values = topology.get_init_heights()[topology.node_mask]
     logp = height_dist.logp(height_values).eval()
     assert_allclose(logp, logp_expected)
+
+skygrid_cases = [
+    ([10.0, 15.0, 12.5, 8.0, 7.0], 1.0, -7.009005278982137),
+    ([2.9, 2.8, 2.7, 2.6, 2.5, 2.4, 2.3, 2.2, 2.1, 2.0], 2.0, -3.444917777278568),
+    ([112.0, 10.2, 12.3], 1.5, -11.774555280807585)
+]
+@pytest.mark.parametrize('population_sizes,cutoff,logp_expected', skygrid_cases)
+def test_coalescent_skygrid(heterochronous_newick, population_sizes, cutoff, logp_expected):
+    tree = newick.loads(heterochronous_newick)[0]
+    topology = TreeTopology(tree)
+    grid_size = len(population_sizes) - 1
+    population_function = GridPopulationFunction(topology, population_sizes, cutoff, grid_size)
+    height_dist = CoalescentTree.dist(topology, population_function)
+    height_values = topology.get_init_heights()[topology.node_mask]
+    logp = height_dist.logp(height_values).eval()
+    assert_allclose(logp, logp_expected)
+
